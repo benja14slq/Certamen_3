4888fcd2de6ca69d822094ffd194e3c9fe52e2ef
benja14slq <benjamin.betty@sansano.usm.cl>
2023-11-23 13:15:50 -0300

    Se realizo el models.py

--- CERT3/CERT3/urls.py
+++ CERT3/CERT3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Certamen3 project.
+URL configuration for CERT3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- CERT3/app/forms.py
+++ CERT3/app/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Evento
+from .models import Evento, Segmento
 
 class EventoForm(forms.ModelForm):
     class meta:
         model = Evento
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['segmento'].queryset = Segmento.objects.filter(
+            segmento_nombre__in=[choice[0] for choice in Segmento.SEGMENTOS_CHOICES]
+        )
