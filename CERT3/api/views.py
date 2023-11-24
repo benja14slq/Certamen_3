f3b6dd81cb7a79615b46160c7505c9879c1b302f
benja14slq <benjamin.betty@sansano.usm.cl>
2023-11-24 14:50:52 -0300

    Commit
    Cree el api rest, un prototipo de la página web

--- CERT3/api/views.py
+++ CERT3/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from app.models import Evento
+from .serializers import EventoSerializer
 
 # Create your views here.
+
+class EventoViewSet(viewsets.ModelViewSet):
+    serializer_class = EventoSerializer
+
+    def get_queryset(self):
+        queryset = Evento.objects.all()
+        year = self.request.query_params.get('year', None)
+        type = self.request.query_params.get('type', None)
+        segment = self.request.query_params.get('segment', None)
+
+        if year:
+            queryset = queryset.filter(start_date__year=year)
+        if type:
+            queryset = queryset.filter(type=type)
+        if segment:
+            queryset = queryset.filter(segment=segment)
+
+        return queryset
